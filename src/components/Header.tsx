6cdb9871b654a95294a701fc6f302d78d37634d8
Peter-Binate <peter.binate@gmail.com>
2024-12-19 15:22:01 +0100

    delete files header and navabar

--- src/components/Header.tsx
+++
@@ -1,8 +0,0 @@
-export default function Header() {
-    return (
-      <header className="h-16 bg-gray-100 flex items-center justify-center shadow">
-        <h1 className="text-lg font-semibold">Mon App</h1>
-      </header>
-    )
-  }
-  
